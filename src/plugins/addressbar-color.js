55a468f57f2dc047bb0b21caac4a929f9398c04a
Diego Hernandes <diego@hernandev.com>
2018-05-07 03:40:24 -0300

    address bar color plugin

---
+++ src/plugins/addressbar-color.js
@@ -0,0 +1,8 @@
+// imports.
+import { AddressbarColor } from 'quasar'
+
+// export plugin.
+export default () => {
+  // almost useless address bar color setter.
+  AddressbarColor.set('#ffffff')
+}
