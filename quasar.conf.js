c2de3c290d8d6a31fbc7414e4904b9cb9ebafab7
Diego Hernandes <diego@hernandev.com>
2018-05-17 17:39:16 -0300

    temporary quasar config (needs cleanup)

--- quasar.conf.js
+++ quasar.conf.js
@@ -1,5 +1,6 @@
-// Configuration for your app
-
+// Configuration for your app.
+const parsed = require('dotenv').load()
+const envVars = parsed['parsed']
 module.exports = function (ctx) {
   return {
     // app plugins (/src/plugins)
@@ -9,18 +10,13 @@ module.exports = function (ctx) {
       'i18n',
       'axios',
       'vuex-router-sync',
-      'moment',
-      'steem',
-      'vault'
+      'bootstrap'
     ],
     css: [
       'app.styl'
     ],
     extras: [
       ctx.theme.mat ? 'roboto-font' : null,
-      'material-icons',
-      // 'ionicons',
-      // 'mdi',
       'fontawesome'
     ],
     supportIE: false,
@@ -29,8 +25,14 @@ module.exports = function (ctx) {
       remove: []
     },
     build: {
+      env: {
+        SC2_APP: envVars['SC2_APP'],
+        SC2_CALLBACK: envVars['SC2_CALLBACK']
+      },
       scopeHoisting: true,
+      // vueRouterMode: 'hash',
       vueRouterMode: 'history',
+      // publicPath: '',
       // gzip: true,
       // analyze: true,
       // extractCSS: false,
@@ -42,16 +44,13 @@ module.exports = function (ctx) {
           loader: 'eslint-loader',
           exclude: /(node_modules|quasar)/
         })
-        cfg.module.rules.push({
-          test: /\.wasm$/,
-          loaders: ['wasm-loader']
-        })
+        cfg.resolve.alias['lodash'] = 'lodash-es'
       }
     },
     devServer: {
       // https: true,
-      // port: 8080,
-      open: true // opens browser window automatically
+      port: 8080,
+      open: false // no auto browser.
     },
     // framework: 'all' --- includes everything; for dev only!
     framework: {
@@ -62,22 +61,39 @@ module.exports = function (ctx) {
         'QLayoutFooter',
         'QTabs',
         'QTab',
+        'QDatetimePicker',
+        'QDatetime',
+        'QDatetime',
         'QTabPane',
         'QRouteTab',
         'QLayoutDrawer',
+        'QChip',
+        'QBtnDropdown',
         'QList',
+        'QSelect',
         'QListHeader',
         'QItem',
+        'QInput',
+        'QPullToRefresh',
+        'QField',
+        'QFab',
+        'QFabAction',
+        'QTooltip',
+        'QPageSticky',
+        'QScrollArea',
         'QInfiniteScroll',
         'QSpinnerDots',
         'QItemMain',
+        'QAjaxBar',
         'QItemSeparator',
         'QItemSide',
         'QItemTile',
         'QPageContainer',
         'QPage',
+        'QSearch',
         'QToolbar',
         'QToolbarTitle',
+        'QSlideTransition',
         'QBtn',
         'QScrollArea',
         'QIcon',
@@ -85,10 +101,12 @@ module.exports = function (ctx) {
         'QListHeader',
         'QItem',
         'QItemMain',
-        'QItemSide'
+        'QItemSide',
+        'QBtnToggle'
       ],
       directives: [
-        'Ripple'
+        'Ripple',
+        'CloseOverlay'
       ],
       // Quasar plugins
       plugins: [
@@ -97,9 +115,8 @@ module.exports = function (ctx) {
         'AddressbarColor'
       ]
     },
-    // animations: 'all' --- includes all animations
-    animations: [
-    ],
+    animations: 'all', //
+    // animations: [],
     pwa: {
       cacheExt: 'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
       manifest: {
@@ -140,23 +157,11 @@ module.exports = function (ctx) {
       }
     },
     cordova: {
-      id: 'solutions.blocker.steembox'
+      id: 'io.utopian.app'
     },
     electron: {
-      extendWebpack (cfg) {
-        // do something with cfg
-      },
-      packager: {
-        // icon: 'src-electron/icons/linux-256x256.png'
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-
-        // Window only
-        // win32metadata: { ... }
-      }
+      extendWebpack (cfg) { },
+      packager: { }
     },
 
     // leave this here for Quasar CLI
