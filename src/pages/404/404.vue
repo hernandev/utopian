ddb9827275e84b528f8e5603f47414312f9cb1b7
Diego Hernandes <diego@hernandev.com>
2018-05-17 17:47:08 -0300

    404 page

---
+++ src/pages/404/404.vue
@@ -0,0 +1,7 @@
+<!-- 404 component. -->
+
+<!-- import component template. -->
+<template src="./404.pug" lang="pug"></template>
+
+<!-- import component styles. -->
+<style src="./404.styl" lang="stylus"></style>
