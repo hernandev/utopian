70d4b4f351eba3eb7e98045df7ff09ce3e640caa
Diego Hernandes <diego@hernandev.com>
2018-05-17 17:44:32 -0300

    app wrapper component

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!-- app wrapper component. -->
+
+<!-- component script -->
+<script>
+// component export.
+export default {
+  // component name.
+  name: 'u-app'
+}
+</script>
+
+<!-- import component template. -->
+<template lang="pug">
+  // wrapper element.
+  div.u-app#q-app
+    // router view enabler.
+    router-view
+</template>
