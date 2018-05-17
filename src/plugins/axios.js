1e3db3190b9e14f729de2f50074e3cdbaed8d37c
Diego Hernandes <diego@hernandev.com>
2018-05-17 17:26:30 -0300

    axios plugin

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,8 +1,22 @@
 // imports.
 import axios from 'axios'
+import { get } from 'lodash-es'
+
+// create an axios instance.
+export const http = axios.create()
+
+// flatten success responses body.
+const flattenSuccess = response => get(response, 'data', {})
+
+// flatten error responses body.
+const flattenError = error => Promise.reject(get(error, 'response.data', {}))
 
 // export plugin.
 export default ({ Vue }) => {
-  // define axios as a component property.
-  Vue.prototype.$axios = axios
+  // success and error interceptors.
+  http.interceptors.response.use(flattenSuccess, flattenError)
+  // expose the $axios instance as $axios on components.
+  Vue.prototype.$axios = http
+  // expose the $http instance as $axios on components.
+  Vue.prototype.$http = http
 }
