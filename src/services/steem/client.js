f8a17d855134d8771c870bb6a010387fc008955d
Diego Hernandes <diego@hernandev.com>
2018-05-17 18:00:52 -0300

    temporary exports for steem client

---
+++ src/services/steem/client.js
@@ -0,0 +1,20 @@
+// imports.
+import client from '@steemit/steem-js'
+
+// named export.
+export const steem = client
+
+// api export.
+export const api = client.api
+
+// auth export.
+export const auth = client.auth
+
+// utils export.
+export const utils = client.utils
+
+// formatter export.
+export const formatter = client.formatter
+
+// default export.
+export default client
