df3a082ab53bdd67af2fa1ba727df68af23c5b6c
Diego Hernandes <diego@hernandev.com>
2018-05-18 06:03:23 -0300

    post author component

---
+++ src/components/posts/author/author.vue
@@ -0,0 +1,31 @@
+<script>
+// post author component (author avatar box).
+export default {
+  // component name.
+  name: 'u-post-author',
+
+  // component props.
+  props: {
+    post: {
+      type: Object,
+      default: null
+    },
+    reverse: {
+      type: Boolean,
+      default: () => (false)
+    }
+  },
+
+  // component data.
+  data () {
+    return {}
+  }
+
+}
+</script>
+
+<!-- component template. -->
+<template lang="pug" src="./author.pug"></template>
+
+<!-- component style. -->
+<style lang="stylus" src="./author.styl"></style>
