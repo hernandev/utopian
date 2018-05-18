5ecdd77c7f5341be748ed316b83342a791ba3689
Diego Hernandes <diego@hernandev.com>
2018-05-18 04:41:00 -0300

    dexie initialization (database)

---
+++ src/database/index.js
@@ -0,0 +1,28 @@
+// retrieve the database name.
+import Dexie from 'dexie'
+import { each } from 'lodash-es'
+import versions from './versions'
+
+// database name (alias).
+export const getDBName = () => 'utopian-db'
+
+// init DB
+export const initDb = () => {
+  // start a database instance.
+  const DB = new Dexie(getDBName())
+  // loop through database versions.
+  each(versions, version => DB.version(version.version).stores(version.stores))
+  // returns the database instance itself.
+  return DB
+}
+
+// call the database init function.
+export const DB = initDb()
+
+// export stores.
+export const secrets = DB.table('secrets')
+export const preferences = DB.table('preferences')
+export const user = DB.table('user')
+
+// default export.
+export default DB
