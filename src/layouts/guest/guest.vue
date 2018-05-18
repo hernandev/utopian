c6f3b5cdb9f70c2a2b34f476a7e2796e674cd7dc
Diego Hernandes <diego@hernandev.com>
2018-05-18 05:32:09 -0300

    guest template for auth routes and other non-full layout page

---
+++ src/layouts/guest/guest.vue
@@ -0,0 +1,16 @@
+<!-- guest layout component. -->
+
+<!-- component script -->
+<script>
+// component export.
+export default {
+  // component name.
+  name: 'u-layout-guest'
+}
+</script>
+
+<!-- component template. -->
+<template src="./guest.pug" lang="pug"></template>
+
+<!-- component styles. -->
+<style src="./guest.styl" lang="stylus"></style>
