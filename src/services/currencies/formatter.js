fb2d626aa78f89a3b2d838d6b6ca9c36f6616fe8
Diego Hernandes <diego@hernandev.com>
2018-05-18 04:39:23 -0300

    currency formatter helper

---
+++ src/services/currencies/formatter.js
@@ -0,0 +1,8 @@
+// helper for currency formatting.
+export const parseCurrencyString = (valueString, minFraction = 0, maxFraction = 2, locale = undefined) => {
+  return parseFloat(valueString)
+    .toLocaleString(locale, {
+      minimumFractionDigits: minFraction,
+      maximumFractionDigits: maxFraction
+    })
+}
