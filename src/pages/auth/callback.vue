fc312247a2d76dcc60e7020454b5044f7fef0150
Diego Hernandes <diego@hernandev.com>
2018-05-18 06:09:45 -0300

    auth callback component

---
+++ src/pages/auth/callback.vue
@@ -0,0 +1,44 @@
+<!-- auth callback (oauth) -->
+
+<!-- component script -->
+<script>
+// imports.
+import { mapActions } from 'vuex'
+import { get } from 'lodash-es'
+
+// auth callback component.
+export default {
+
+  // component name,
+  name: 'u-auth-callback',
+
+  // component methods.
+  methods: {
+
+    // map auth store actions.
+    ...mapActions('auth', [
+      'loginWithCallback'
+    ]),
+
+    // parse oauth callback query.
+    parseQuery () {
+      return this.loginWithCallback(get(this.$route, 'query'))
+    }
+  },
+
+  // mounted hook.
+  mounted () {
+    // call the parse query method upon render.
+    return this.parseQuery()
+      .then(() => {
+        // after, redirect home after login.
+        this.$router.push({ name: 'home' })
+      })
+  }
+}
+</script>
+
+<!-- component template -->
+<template lang="pug">
+  div
+</template>
