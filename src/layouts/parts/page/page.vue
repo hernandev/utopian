d0afbb52d81d5b4aa5674b430469b5a5507cafbd
Diego Hernandes <diego@hernandev.com>
2018-05-18 05:35:04 -0300

    layout page wrapper component

---
+++ src/layouts/parts/page/page.vue
@@ -0,0 +1,79 @@
+<!-- custom page wrapper for the application. -->
+
+<!-- component script -->
+<script>
+// imports.
+import { mapGetters, mapMutations } from 'vuex'
+
+// component export.
+export default {
+
+  // component name.
+  name: 'u-layout-page',
+
+  // component data.
+  data () {
+    return {}
+  },
+
+  // computed properties.
+  computed: {
+
+    // map common getters.
+    ...mapGetters('common', [
+      'drawerOpen',
+      'drawerCollapsed',
+      'isMobile',
+      'isDesktop'
+    ]),
+
+    // open / close drawer v-model bypass.
+    open: {
+      get: function () {
+        return this.drawerOpen
+      },
+      set: function (v) {
+        return this.setDrawerOpen(v)
+      }
+    },
+
+    // dynamic css classes for the drawer.
+    drawerClasses () {
+      if (this.isMobile) {
+        return [ 'u-drawer' ]
+      }
+      // on case not mobile, return the collapsed class, if collapsed
+      return this.drawerCollapsed ? [ 'u-drawer', 'collapsed' ] : [ 'u-drawer' ]
+    }
+  },
+
+  // component methods.
+  methods: {
+    ...mapMutations('common', [
+      'setDrawerCollapsed',
+      'setDrawerOpen'
+    ]),
+
+    // toggle the collapse state.
+    toggleCollapse () {
+      this.setDrawerCollapsed()
+    },
+
+    // expand when the mouse enters the drawer
+    mouseOver () {
+      this.setDrawerCollapsed(false)
+    },
+
+    // retreat when the mouse leaves.
+    mouseLeave () {
+      this.setDrawerCollapsed(true)
+    }
+  }
+}
+</script>
+
+<!-- component template. -->
+<template src="./page.pug" lang="pug"></template>
+
+<!-- component styles. -->
+<style src="./page.styl" lang="stylus"></style>
