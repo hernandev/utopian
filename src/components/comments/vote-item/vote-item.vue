c75fd5b6259d4b7e5ced81edf69c6fafd54aa5b0
Diego Hernandes <diego@hernandev.com>
2018-05-21 13:04:34 -0300

    vote-item component

---
+++ src/components/comments/vote-item/vote-item.vue
@@ -0,0 +1,68 @@
+<script>
+// imports.
+import { mapGetters } from 'vuex'
+import { get } from 'lodash-es'
+import { parseCurrencyString } from 'src/services/currencies/formatter'
+
+// post author component (author avatar box).
+export default {
+  // component name.
+  name: 'u-comments-vote-item',
+
+  // component props.
+  props: {
+    // vote item data.
+    vote: {
+      type: Object,
+      default: null
+    }
+  },
+
+  // component data.
+  data () {
+    return {}
+  },
+
+  // computed properties.
+  computed: {
+
+    // map steem store getters.
+    ...mapGetters('steem', [
+      'voteDivisor',
+      'recentClaims',
+      'rewardBalance',
+      'baseFeedPrice'
+    ]),
+
+    // voter name.
+    voter () {
+      return get(this.vote, 'voter', null)
+    },
+
+    // voter avatar.
+    avatar () {
+      return `https://img.blocker.press/a/${this.voter}`
+    },
+
+    // vote reward shares.
+    rewardShares () {
+      return parseInt(get(this.vote, 'rshares', 0))
+    },
+
+    // calculates how much the vote is worth.
+    voteValue () {
+      // get the vote value (float value).
+      const voteValue = (this.rewardShares / this.recentClaims * this.rewardBalance * this.baseFeedPrice)
+      // parse as currency.
+      return parseCurrencyString(voteValue, 2, 2)
+    }
+  }
+
+}
+</script>
+
+<!-- component template. -->
+<template lang="pug" src="./vote-item.pug"></template>
+
+<!-- component style. -->
+<style lang="stylus" src="./vote-item.styl"></style>
