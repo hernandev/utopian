ae9be68b1d73748d0e8da40645d4a2dcc66cda0f
Diego Hernandes <diego@hernandev.com>
2018-06-01 15:20:09 -0300

    updating config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     'import/extensions': 0,
     'import/no-unresolved': 0,
     'import/no-extraneous-dependencies': 0,
+    'import/no-webpack-loader-syntax': 0,
 
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
