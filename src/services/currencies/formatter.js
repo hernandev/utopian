574283c5c095bd8ba36f0df787450a337b0fdbd2
Diego Hernandes <diego@hernandev.com>
2018-06-04 09:43:59 -0300

    currency formatter changes (WIP)

--- src/services/currencies/formatter.js
+++ src/services/currencies/formatter.js
@@ -1,8 +1,8 @@
+// import { currency } from '@blocker/steem-formatter'
 // helper for currency formatting.
 export const parseCurrencyString = (valueString, minFraction = 0, maxFraction = 2, locale = undefined) => {
-  return parseFloat(valueString)
-    .toLocaleString(locale, {
-      minimumFractionDigits: minFraction,
-      maximumFractionDigits: maxFraction
-    })
+  return parseFloat(valueString).toLocaleString(locale, {
+    minimumFractionDigits: minFraction,
+    maximumFractionDigits: maxFraction
+  })
 }
