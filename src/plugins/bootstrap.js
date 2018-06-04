03215161384e9c2aadfd1a412cb78115cc8e4839
Diego Hernandes <diego@hernandev.com>
2018-06-04 09:19:52 -0300

    bootstrap plugin

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -1,5 +1,19 @@
+// imports.
+import ULayoutPage from 'src/layouts/parts/page/page'
+import UPostReplyWrapper from 'src/components/posts/reply/wrapper'
+import UPostReply from 'src/components/posts/reply/reply'
+
 // main application bootstrap (before router init methods).
 export default ({ app, router, Vue, store }) => {
-  // dispatch common bootstrap action on store.
+  // register globally available components.
+  Vue.component('u-layout-page', ULayoutPage)
+  Vue.component('u-post-reply-wrapper', UPostReplyWrapper)
+  Vue.component('u-post-reply', UPostReply)
+
+  // dispatch common actions.
+  // @TODO move to common bootstrap action.
   store.dispatch('common/bootstrap')
+  store.dispatch('steem/loadRewardFund')
+  store.dispatch('steem/loadDynamicProperties')
+  store.dispatch('steem/loadFeedPrice')
 }
