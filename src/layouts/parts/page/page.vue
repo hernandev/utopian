3db889ee46046a29a971382dd02509d358f812e8
Diego Hernandes <diego@hernandev.com>
2018-06-04 09:37:27 -0300

    toolbar and page layout components refactor (WIP)

--- src/layouts/parts/page/page.vue
+++ src/layouts/parts/page/page.vue
@@ -3,7 +3,7 @@
 <!-- component script -->
 <script>
 // imports.
-import { mapGetters, mapMutations } from 'vuex'
+import { mapGetters } from 'vuex'
 
 // component export.
 export default {
@@ -30,53 +30,14 @@ export default {
 
     // map common getters.
     ...mapGetters('common', [
-      'drawerOpen',
-      'drawerCollapsed',
       'isMobile',
       'isDesktop'
-    ]),
-
-    // open / close drawer v-model bypass.
-    open: {
-      get: function () {
-        return this.drawerOpen
-      },
-      set: function (v) {
-        return this.setDrawerOpen(v)
-      }
-    },
-
-    // dynamic css classes for the drawer.
-    drawerClasses () {
-      if (this.isMobile) {
-        return [ 'u-drawer' ]
-      }
-      // on case not mobile, return the collapsed class, if collapsed
-      return this.drawerCollapsed ? [ 'u-drawer', 'collapsed' ] : [ 'u-drawer' ]
-    }
+    ])
   },
 
   // component methods.
   methods: {
-    ...mapMutations('common', [
-      'setDrawerCollapsed',
-      'setDrawerOpen'
-    ]),
-
-    // toggle the collapse state.
-    toggleCollapse () {
-      this.setDrawerCollapsed()
-    },
-
-    // expand when the mouse enters the drawer
-    mouseOver () {
-      this.setDrawerCollapsed(false)
-    },
-
-    // retreat when the mouse leaves.
-    mouseLeave () {
-      this.setDrawerCollapsed(true)
-    }
+    //
   }
 }
 </script>
---
+++ src/layouts/parts/toolbar/toolbar.vue
@@ -0,0 +1,68 @@
+<!-- component script -->
+<script>
+// imports.
+import { mapGetters, mapActions } from 'vuex'
+
+// layout toolbar component.
+export default {
+
+  // component name.
+  name: 'u-layout-toolbar',
+
+  // component data.
+  data () {
+    return {
+      overlay: null
+    }
+  },
+
+  // computed properties.
+  computed: {
+
+    // auth store getters.
+    ...mapGetters('auth', [
+      'guest',
+      'username'
+    ]),
+
+    // common store getters.
+    ...mapGetters('common', [
+      'isMobile',
+      'isDesktop'
+    ]),
+
+    // avatar image URL.
+    avatar () {
+      return this.guest ? null : ('https://img.blocker.press/a/' + this.username)
+    },
+
+    createLabel () {
+      return this.isDesktop ? 'Contribution' : ''
+    }
+  },
+
+  // component methods.
+  methods: {
+
+    ...mapActions('auth', [
+      'logout'
+    ]),
+
+    // redirect to create route.
+    redirectToCreate () {
+      return this.$router.push({ name: 'create' })
+    },
+
+    // redirect to login route.
+    redirectToLogin () {
+      return this.$router.push({ name: 'auth.login' })
+    }
+  }
+}
+</script>
+
+<!-- component template. -->
+<template src="./toolbar.pug" lang="pug"></template>
+
+<!-- component styles. -->
+<style src="./toolbar.styl" lang="stylus"></style>
