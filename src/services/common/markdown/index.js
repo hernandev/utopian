ddbed94e99722edbc610d9b1a4f8360774dcb520
Diego Hernandes <diego@hernandev.com>
2018-06-04 08:50:00 -0300

    module export

---
+++ src/services/common/markdown/index.js
@@ -0,0 +1,8 @@
+// imports.
+import * as markdown from './markdown'
+import * as sanitizer from './sanitizer'
+
+// export both markdown parser and sanitize helpers.
+export default {
+  markdown, sanitizer
+}
